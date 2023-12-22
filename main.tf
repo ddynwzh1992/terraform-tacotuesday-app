8d8fc350ef79f1f4204656509bce63f9ea595045
Vincent <ddynvincent@gmail.com>
2023-12-22 17:19:29 +0800

    Add third subnet and tags

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ module "eks" {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t3.small"]
+      instance_types = ["t4g.small"]
 
       min_size     = 1
       max_size     = 3
